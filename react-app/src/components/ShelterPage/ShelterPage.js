18916c9cd0c77a1f23076bf3ddae13c210ab13c0
nathaniel <nathanieljortega@gmail.com>
2021-05-14 22:58:44 -0500

    setting up shelter info on the shelter page and got shelters animals rendered on the page

--- react-app/src/components/ShelterPage/ShelterPage.js
+++ react-app/src/components/ShelterPage/ShelterPage.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import SmallAnimalTile from '../SmallAnimalTile/SmallAnimalTile';
 import { getOneShelter } from '../../store/shelters';
+import SmallAnimalTile from '../SmallAnimalTile/SmallAnimalTile';
+import emailIcon from '../../public/images/email.png';
+import phoneIcon from '../../public/images/phone.png';
+import MapContainer from '../MapContainer/MapContainer';
+import mapLocation from '../../public/images/location.png';
 import './ShelterPage.css';
 
 
@@ -14,19 +18,46 @@ function ShelterPage() {
 
     useEffect(() => {
         dispatch(getOneShelter(shelterId))
-    }, [dispatch])
+    }, [dispatch, shelterId])
 
-    const sheltersAnimals = useSelector(state => state.shelters?.shelters)
-    if (!sheltersAnimals) return null;
+    const shelter = useSelector(state => state.shelters?.shelters)
+    if (!shelter) return null;
 
-    const animals = sheltersAnimals[0].animals
+    const shelterInfo = shelter[0];
+    console.log(shelterInfo)
+
+    const animals = shelter[0].animals
 
     return (
-        <div className="shelter-animal-list">
-            {animals.map((animal, i) => (
-                <SmallAnimalTile animal={animal} key={i} />
-            ))}
-        </div>
+        <>
+            <div className="shelter-info-container">
+                <div className="shelter-img">
+                    <img src={shelterInfo.image_url} alt={"👽"} className="shelter-icon"/>
+                </div>
+                <div className="shelter-description">
+                    <div>
+                        <MapContainer shelter={shelterInfo}/>
+                    </div>
+                    <img src={mapLocation} className="location-icon" alt={"👽"}/>
+                    <div className="shelter-address">
+                        {shelterInfo.address}
+                    </div>
+                    <div className="shelter-email">
+                        <img src={emailIcon} className="email-icon" alt={"👽"}/>
+                        {shelterInfo.email}
+                    </div>
+                    <div className="shelter-phone">
+                        <img src={phoneIcon} className="phone-icon" alt={"👽"}/>
+                        {shelterInfo.phone_number}
+                    </div>
+                </div>
+            </div>
+            <div className="shelter-animal-list">
+                {animals.map((animal, i) => (
+                    <SmallAnimalTile animal={animal} key={i} />
+                ))}
+            </div>
+        </>
     )
 }
 
