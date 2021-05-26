81340911fbae549c034ff9977b629008607189f2
nathaniel <nathanieljortega@gmail.com>
2021-05-26 12:37:46 -0500

    cleaned up some of the commented out code that was not needed

--- react-app/src/components/UpdateUserFormModal/UpdateUserForm.js
+++ react-app/src/components/UpdateUserFormModal/UpdateUserForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { updateUser } from '../../store/session';
 import { hideUploadModal } from '../../store/modals';
